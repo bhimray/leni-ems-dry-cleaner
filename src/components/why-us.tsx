0a1022084ab559fe7c2ef9d6aee7e7379a0b8b29
bilenray <raybimlendra@gmail.com>
2022-09-15 11:48:54 +1000

    modified:   src/components/why-us.tsx

--- src/components/why-us.tsx
+++ src/components/why-us.tsx
@@ -5,14 +5,13 @@ import Helmet from "react-helmet";
 export interface  WhyusProps {
 }
 
-
 export default function Whyus (props:  WhyusProps) {
   const review= useScript("https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247")
   console.log("review",review)
   return (
     <div className='customers-review'>
       <div className='customer-review-head'>Customer's Review</div>
-      <div className='customer-review-headlines'>Rated 4.5/5 by 1000+ customers</div>
+      <div className='customer-review-headlines'>Rated 4.5/5 by 100+ customers</div>
       <Helmet>
         <script defer async src='https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247'></script>
       </Helmet>
