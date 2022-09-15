2425addf13407e20ef44a737286eb4bd8fba7487
YugendraRay <ray.yuge@gmail.com>
2022-09-15 11:17:00 +1000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/why-us.tsx 	new file:   src/hooks/useScript.jsx 	modified:   src/react-app-env.d.ts 	modified:   tsconfig.json

--- src/components/why-us.tsx
+++ src/components/why-us.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import '../styles/why-us.css'
+import useScript from '../hooks/useScript';
+import Helmet from "react-helmet";
 export interface  WhyusProps {
 }
-<script defer async src='https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247'></script>
+
+
 export default function Whyus (props:  WhyusProps) {
+  const review= useScript("https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247")
+  console.log("review",review)
   return (
     <div className='customers-review'>
       <div className='customer-review-head'>Customer's Review</div>
       <div className='customer-review-headlines'>Rated 4.5/5 by 1000+ customers</div>
-      <div className='customer-review'><script defer async src='https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247'></script></div>
+      <Helmet>
+        <script defer async src='https://cdn.trustindex.io/loader.js?74951f191e8987333d50ce9247'></script>
+      </Helmet>
       <div className='customer-review-see-more'></div>
     </div>
   );
